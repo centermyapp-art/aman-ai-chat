c130996c222e3763e409da28d49101cea2aa6d3f
Aman Sharma <centermyapp@gmail.com>
2026-06-14 15:54:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Aman AI is running!"
+    return "Aman AI is running successfully!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
